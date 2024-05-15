8795cc1831290aaaf91828ef26b25b6393f53ad0
Ale241302 <128639865+Ale241302@users.noreply.github.com>
2024-05-15 16:20:48 -0500

    actualizacion

--- Sp/hikashop/wordtoimage/wordtoimage.php
+++ Sp/hikashop/wordtoimage/wordtoimage.php
@@ -12,7 +12,6 @@ class plgHikashopWordtoimage extends hikashopPlugin {
     public $name = 'wordtoimage';
 
     public function onPluginConfiguration(&$element) {
-        // Aquí puedes realizar configuraciones adicionales si es necesario
         return $this->pluginConfiguration($element);
     }
 
@@ -21,19 +20,22 @@ class plgHikashopWordtoimage extends hikashopPlugin {
         $uri = new Joomla\CMS\Uri\Uri($app->input->server->get('REQUEST_URI'));
         $currentUrl = $uri->toString();
         $language = JFactory::getLanguage()->getTag();
-        echo $language;
-        if (strpos($currentUrl, '/administrator/index.php?option=com_hikashop&ctrl=product')!== false) {
-            return;
+
+        // Check if we're in the administrator side
+        if ($app->isClient('administrator')) {
+            if (strpos($currentUrl, 'index.php?option=com_hikashop&ctrl=product') !== false) {
+                return;
+            }
+        } else {
+            $renderedContent = $this->getRenderedContent();
+            $pluginParams = $this->getPluginParams();
+
+            if ($pluginParams) {
+                $this->replaceWordsWithImages($renderedContent, $pluginParams);
+            }
+
+            $app->setBody($renderedContent);
         }
-
-        $renderedContent = $this->getRenderedContent();
-        $pluginParams = $this->getPluginParams();
-
-        if ($pluginParams) {
-            $this->replaceWordsWithImages($renderedContent, $pluginParams);
-        }
-
-        $app->setBody($renderedContent);
     }
 
     private function getRenderedContent() {
@@ -67,36 +69,39 @@ class plgHikashopWordtoimage extends hikashopPlugin {
     private function replaceWordsWithImages(&$content, $pluginParams) {
         $wordToReplace = $pluginParams->word_to_replace;
         $imageToInsert = $pluginParams->image_to_insert;
-        $link = isset($pluginParams->link)? $pluginParams->link : '';
+        $link = isset($pluginParams->link) ? $pluginParams->link : [];
         $language = JFactory::getLanguage()->getTag();
+        
         if (is_array($wordToReplace)) {
             foreach ($wordToReplace as $index => $word) {
-                if (strpos($content, $word)!== false) {
-                    $pattern = '/<(input[^>]*type="(?:text|select|radio|checkbox)"[^>]*value="([^"]*'.$word.'[^"]*)"[^>]*|select[^>]*type="list"[^>]*value="'.$word.'"[^>]*|select[^>]*value="'.$word.'"[^>]*|input[^>]*type="radio"[^>]*name="[^"]*'.$word.'[^"]*"[^>]*|input[^>]*type="checkbox"[^>]*name="[^"]*'.$word.'[^"]*"[^>]*|textarea[^>]*>'.$word.'<\/textarea>|<div[^>]*class="hikashop_filter_module"[^>]*>'.$word.'<\/div>)/i';
-                    if (preg_match($pattern, $content, $matches)) {
-                        continue;
-                    }
-                    $wordpage = $word . "_" . $language;
-                    if ($wordpage == 'Dielectrico_es-ES') {
-                        $linkpage = "https://mwt.one/index.php?option=com_sppagebuilder&view=page&id=41";
-                    } elseif ($wordpage == 'Dielectrico_en-US') {
-                        $linkpage = "https://mwt.one/index.php?option=com_sppagebuilder&view=page&id=42";
-                    } else {
-                        $linkpage = $link[$index];
-                    }
-                    
-                    $imageTag = '<a href="'.  $linkpage. '" target="_blank"><img src="'. $imageToInsert[$index]. '" alt="'. $word.'"  title="' . $word . '" width="50" height="50" style="display: inline-block;"/></a>';
-                    $content = str_replace($word, $imageTag, $content);
-                }
+                // Define el patrón de búsqueda excluyendo las etiquetas <a>, <li>, <select> y <option>
+                $pattern = '/(<(a|li|select|option)[^>]*>.*?<\/\2>(*SKIP)(*FAIL))|' . preg_quote($word, '/') . '/i';
+                $content = preg_replace_callback(
+                    $pattern,
+                    function ($matches) use ($word, $imageToInsert, $link, $index, $language) {
+                        // Si coincide con una etiqueta <a>, <li>, <select> o <option>, devolver la coincidencia sin cambios
+                        if (!empty($matches[1])) {
+                            return $matches[1];
+                        }
+    
+                        $wordpage = $word . "_" . $language;
+    
+                        // Determine el enlace apropiado
+                        if ($wordpage == 'Dielectrico_es-ES') {
+                            $linkpage = "https://mwt.one/index.php?option=com_sppagebuilder&view=page&id=41";
+                        } elseif ($wordpage == 'Dielectrico_en-US') {
+                            $linkpage = "https://mwt.one/index.php?option=com_sppagebuilder&view=page&id=42";
+                        } else {
+                            $linkpage = isset($link[$index]) ? $link[$index] : '#';
+                        }
+    
+                        // Genera la etiqueta de imagen
+                        $imageTag = '<a href="' . $linkpage . '" target="_blank"><img src="' . $imageToInsert[$index] . '" alt="' . $word . '" title="' . $word . '" width="50" height="50" style="display: inline-block;"/></a>';
+                        return $imageTag;
+                    },
+                    $content
+                );
             }
-        } else {
-            $pattern = '/<(input[^>]*type="(?:text|select|radio|checkbox)"[^>]*value="([^"]*'.$wordToReplace.'[^"]*)"[^>]*|select[^>]*type="list"[^>]*value="'.$wordToReplace.'"[^>]*|select[^>]*value="'.$wordToReplace.'"[^>]*|input[^>]*type="radio"[^>]*name="[^"]*'.$wordToReplace.'[^"]*"[^>]*|input[^>]*type="checkbox"[^>]*name="[^"]*'.$wordToReplace.'[^"]*"[^>]*|textarea[^>]*>'.$wordToReplace.'<\/textarea>|<div[^>]*class="hikashop_filter_module"[^>]*>'.$wordToReplace.'<\/div>)/i';
-            if (preg_match($pattern, $content, $matches)) {
-                return;
-            }
-
-            $imageTag = '<a href="'. $link . '" target="_blank"><img src="'. $imageToInsert . '" alt="'. $wordToReplace.'"  title="' . $word . '" style="display: inline-block;"/></a>';
-            $content = str_replace($wordToReplace, $imageTag, $content);
         }
-    }
+    }   
 }
